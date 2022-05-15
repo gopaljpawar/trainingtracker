080b6f4d40e995a5a75ef4adff514cd43318eff4
gopaljpawar <gopalpawar332@gmail.com>
2022-05-15 07:50:02 +0530

    gor new apps

---
+++ TestApp/src/com/nt/apps/Manager.java
@@ -0,0 +1,12 @@
+package com.nt.apps;
+
+public class Manager {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome to Hindustan bro");
+
+	}
+
+}
