c9d50be763a725bb9da5c387a2dc6b9be605e106
pawargopal <pawargopal332@gmail.com>
2022-05-15 09:08:10 +0530

    this for tiger app changes

--- TestDemo2/src/cpm/nt/tigers/TigerApp.java
+++ TestDemo2/src/cpm/nt/tigers/TigerApp.java
@@ -2,10 +2,14 @@ package cpm.nt.tigers;
 
 public class TigerApp {
 	
+	static void m1() {
+		System.out.println("Generated apps");
+	}
 	public static void main(String[] args) {
 		
 		System.out.println("this final demo");
 	
+		
 	}
 
 }
